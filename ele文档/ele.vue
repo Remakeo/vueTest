277f44984cb584f86b17f82ae9740b7a88b01d10
liuzhiying <906918636@qq.com>
2019-07-12 09:21:36 +0800

    js练习题

---
+++ ele文档/ele.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <el-input v-model="temp" placeholder="请输入内容"></el-input>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ele',
+        data() {
+            return {
+                temp: '11111111'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
